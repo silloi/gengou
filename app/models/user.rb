da77f8432c6795af59f7f14add557b10a3631cd8
silloi <silloi1993@gmail.com>
2019-02-04 00:07:05 +0900

    sessions and users authentication equipped

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  def self.find_or_create_from_auth(auth)
+      uid = auth[:uid]
+      user_name = auth[:info][:name]
+      image_url = auth[:info][:image]
+
+      self.find_or_create_by(uid: uid) do |user|
+          user.user_name = user_name
+          user.image_url = image_url
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/generate', to: 'gengou#new'
   get '/about', to: 'static_pages#about'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
